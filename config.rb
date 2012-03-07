da91955677b672dd310826556f380ecd8dd636ed
Beau Smith <beau@beausmith.com>
2012-03-07 01:30:47 -0800

    Convert CSS to Sass (with Compass)

---
+++ config.rb
@@ -0,0 +1,12 @@
+# Compass Config
+# Compass Variables
+http_path = "/" # Set this to the root of your project when deployed.
+css_dir = "views/css"
+sass_dir = "views/sass"
+images_dir = "views/images"
+javascripts_dir = "views/js"
+output_style = :nested # :expanded or :nested or :compact or :compressed
+relative_assets = true
+line_comments = false
+preferred_syntax = :sass
+asset_cache_buster :none
