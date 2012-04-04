fcbcdb35086d0719808d669b81f475ba4fbf7ee7
julien <julien.genestoux@gmail.com>
2012-04-04 14:55:56 +0200

    indicating why we need the Google Auth for signups

--- views/js/signup.js
+++ views/js/signup.js
@@ -11933,15 +11933,31 @@ require.alias("backbone-browserify", "/node_modules/backbone");
 
 require.define("/signup.js", function (require, module, exports, __dirname, __filename) {
     var Msgboy = require('./msgboy.js').Msgboy;
+var $ = require('jquery');
 
 Msgboy.bind("loaded", function () {
-    var matches = window.location.search.match(/\?u=(.*)\&t=(.*)/);
-    chrome.extension.sendRequest({
-        signature: "register",
-        params: {username: matches[1], token: matches[2]}
-    }, function (response) {
-        window.location = chrome.extension.getURL('/views/html/dashboard.html');
-    });
+    if(window.location.search.match(/\?denied/)) {
+        // The user denied access!
+        $("#intro").html("<h1>Signup</h1>\
+        <p>Unfortunately, we currently <em>need</em> you to \
+        accept the Google OpenId authentication at the moment. \
+        It is required as we need a unique identifier for each \
+        user. </p>\
+        <p>However, please know that <em>we do not ask for any \
+        personal data</em>, and just get a unique identifier from \
+        Google.</p> \
+        <p><a class=\"btn\" id=\"retrySignup\" value=\"Retry\" \
+        href=\"http://msgboy.com/session/new?ext=" + chrome.i18n.getMessage("@@extension_id") +"\">Retry</a></p>");
+    }
+    else {
+        var matches = window.location.search.match(/\?u=(.*)\&t=(.*)/);
+        chrome.extension.sendRequest({
+            signature: "register",
+            params: {username: matches[1], token: matches[2]}
+        }, function (response) {
+            window.location = chrome.extension.getURL('/views/html/dashboard.html');
+        });
+    }
 });
 
 
