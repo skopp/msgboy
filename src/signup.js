d9ee90d0b5181522c712f72469ea1b76ee4b8ffd
julien <julien.genestoux@gmail.com>
2012-04-04 22:53:53 +0200

    changed the wording on the authorization page

--- src/signup.js
+++ src/signup.js
@@ -5,15 +5,15 @@ Msgboy.bind("loaded", function () {
     if(window.location.search.match(/\?denied/)) {
         // The user denied access!
         $("#intro").html("<h1>Signup</h1>\
-        <p>Unfortunately, we currently <em>need</em> you to \
-        accept the Google OpenId authentication at the moment. \
+        <p>We currently <em>need</em> you to accept the Google\
+        OpenId authentication. (and we hope we can change that too.) \
         It is required as we need a unique identifier for each \
         user. </p>\
         <p>However, please know that <em>we do not ask for any \
         personal data</em>, and just get a unique identifier from \
         Google.</p> \
         <p><a class=\"btn\" id=\"retrySignup\" value=\"Retry\" \
-        href=\"http://msgboy.com/session/new?ext=" + chrome.i18n.getMessage("@@extension_id") +"\">Retry</a></p>");
+        href=\"http://msgboy.com/session/new?ext=" + chrome.i18n.getMessage("@@extension_id") +"\">Authorize Msgboy</a></p>");
     }
     else {
         var matches = window.location.search.match(/\?u=(.*)\&t=(.*)/);
