a2442ea1112d19365d41a84170f494063234fa56
julien <julien.genestoux@gmail.com>
2012-04-16 09:59:25 -0400

    the protocol handler doesn't do discovery either

--- src/subscribe.js
+++ src/subscribe.js
@@ -4,18 +4,15 @@ var Feediscovery = require('./feediscovery.js').Feediscovery;
 
 Msgboy.bind("loaded", function () {
     var url =  unescape(unescape(window.location.search).match(/subscribe:(.*)/)[1]);
-    Feediscovery.get(url, function (links) {
-        for(var j = 0; j < links.length; j++) {
-            var link = links[j];
-            chrome.extension.sendRequest({
-                signature: "subscribe",
-                params: {
-                    title: link.title,
-                    url: link.href
-                }
-            }, function (response) {
-                // Done
-            });
+    
+    chrome.extension.sendRequest({
+        signature: "subscribe",
+        params: {
+            title: "",
+            url: url,
+            doDiscovery: true
         }
+    }, function (response) {
+        // Done
     });
 });
