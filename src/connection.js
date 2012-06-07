db3c1cc2701c5014b14636daf00ffdf41eb539b3
julien <julien.genestoux@gmail.com>
2012-06-07 19:53:55 +0200

    more socket.io implementation

--- src/connection.js
+++ src/connection.js
@@ -6,6 +6,8 @@ Connection = function() {
     this.reconnectDelay = 0;
     this.reconnectionTimeout = null;
     this._requests = {};
+    this._stack = [];
+    this._ready = false;
 }
 
 util.inherits(Connection, EventEmitter);
@@ -14,18 +16,17 @@ Connection.prototype.connect = function(endpoint, login, password) {
     console.log("Connecting");
     this._socket = io.connect(endpoint);
     
+    // Socket Open!
     this._socket.on('connect', function() {
-        // Socket Open!
         clearTimeout( this.reconnectionTimeout );
         this.reconnectionTimeout = null;
-        console.log("connected");
-        this.connected = true;
+        console.log("Connected");
         this._socket.emit('auth', { login: login, password: password });
     }.bind(this));
     
+    // Socket Closed
     this._socket.on('disconnect', function() {
-        // Socket closed
-        this.connected = false;
+        this._ready = false;
         this.reconnectDelay = Math.min(this.reconnectDelay + 1, 10); // We max at one attempt every minute. 
         var delay = Math.pow(this.reconnectDelay, 2) * 1000;
         console.log("Disconnected. Retrying in", delay, "ms.");
@@ -37,16 +38,32 @@ Connection.prototype.connect = function(endpoint, login, password) {
         }   
 	}.bind(this));
 	
+    // Message from the server!
     this._socket.on('notification', function(message) {
-        // Message from the server!
+        console.log("Notification!");
         console.log(message)
 	}.bind(this));
 	
+	// We're ready!
+    // And we need to process the stack!
 	this._socket.on('ready', function(data) {
-	    // Ok, well, we're ready now.
-    });
+        console.log("Ready");
+        this._ready = true;
+        while(m = this._stack.shift()) {
+		    this._socket.emit(m[0], m[1]);
+        }
+    }.bind(this));
     
+    // Successfuly subscribed
     this._socket.on('subscribed', function(subs) {
+        if(this._requests[subs.id]) {
+            this._requests[subs.id](true, subs.url);
+            delete this._requests[subs.id];
+        }
+    }.bind(this));
+    
+    // Successfuly unsubscribed
+    this._socket.on('unsubscribed', function(subs) {
         if(this._requests[subs.id]) {
             this._requests[subs.id](true, subs.url);
             delete this._requests[subs.id];
@@ -58,11 +75,23 @@ Connection.prototype.connect = function(endpoint, login, password) {
 Connection.prototype.subscribe = function(url, callback) {
     var requestId = btoa(url).substring(10,20);
     this._requests[requestId] = callback;
-    this._socket.emit('subscribe', {url: url, id: requestId});
+    if(this._ready) {
+        this._socket.emit('subscribe', {url: url, id: requestId});
+    }
+    else {
+        this._stack.push(['subscribe', {url: url, id: requestId}]);
+    }
 }
 
 Connection.prototype.unsubscribe = function(url, callback) {
-    console.log("UNSUBSCRIBE");
+    var requestId = btoa(url).substring(10,20); 
+    this._requests[requestId] = callback;
+    if(this._ready) {
+        this._socket.emit('unsubscribe', {url: url, id: requestId});
+    }
+    else {
+        this._stack.push(['unsubscribe', {url: url, id: requestId}]);
+    }
 }
 
 
