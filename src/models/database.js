f769645cb23b3a7c932b7c0d079f21d49aaa4a9d
julien <julien.genestoux@gmail.com>
2012-12-09 22:22:34 +0100

    refactored the transactions

--- src/models/database.js
+++ src/models/database.js
@@ -4,65 +4,31 @@ var msgboyDatabase = {
     migrations: [{
         version: 1,
         migrate: function (transaction, next) {
-            transaction.db.createObjectStore("messages");
-            transaction.db.createObjectStore("inbox");
-            next();
-        }
-    }, {
-        version: 2,
-        migrate: function (transaction, next) {
-            var store = transaction.objectStore("messages");
-            store.createIndex("createdAtIndex", "createdAt", {
+            var messageStore = transaction.db.createObjectStore("messages");
+            messageStore.createIndex("createdAtIndex", "createdAt", {
                 unique: false
             });
-            next();
-        }
-    }, {
-        version: 3,
-        migrate: function (transaction, next) {
-            var store = transaction.db.createObjectStore("feeds");
-            store.createIndex("urlIndex", "url", {
+            messageStore.createIndex("sourceLinkIndex", "sourceLink", {
                 unique: false
             });
-            next();
-        }
-    }, {
-        version: 4,
-        migrate: function (transaction, next) {
-            var store = transaction.objectStore("messages");
-            store.createIndex("sourceLinkIndex", "sourceLink", {
+            messageStore.createIndex("hostIndex", "sourceHost", {
                 unique: false
             });
-            store.createIndex("hostIndex", "sourceHost", {
+            messageStore.createIndex("stateIndex", "state", {
                 unique: false
             });
-            next();
-        }
-    }, {
-        version: 5,
-        migrate: function (transaction, next) {
-            var store = transaction.objectStore("messages");
-            store.createIndex("stateIndex", "state", {
+            messageStore.createIndex("feedIndex", "feed", {
                 unique: false
             });
-            next();
-        }
-    }, {
-        version: 6,
-        migrate: function (transaction, next) {
-            var store = transaction.objectStore("messages");
-            store.createIndex("feedIndex", "feed", {
+            var inboxStore = transaction.db.createObjectStore("inbox");
+            var feedStore = transaction.db.createObjectStore("feeds");
+            feedStore.createIndex("urlIndex", "url", {
                 unique: false
             });
-            next();
-        }
-    }, {
-        version: 7,
-        migrate: function (transaction, next) {
-            var subscriptions = transaction.db.createObjectStore("subscriptions");
-            subscriptions.createIndex("stateIndex", "state", {unique: false});
-            subscriptions.createIndex("subscribedAtIndex", "subscribedAt", {unique: false});
-            subscriptions.createIndex("unsubscribedAtIndex", "unsubscribedAt", {unique: false});
+            var subscriptionStore = transaction.db.createObjectStore("subscriptions");
+            subscriptionStore.createIndex("stateIndex", "state", {unique: false});
+            subscriptionStore.createIndex("subscribedAtIndex", "subscribedAt", {unique: false});
+            subscriptionStore.createIndex("unsubscribedAtIndex", "unsubscribedAt", {unique: false});
             next();
         }
     }]
